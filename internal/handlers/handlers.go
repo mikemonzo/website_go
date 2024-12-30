c41a0fcaea1d1a06e7940f484e48b85f2a8de8ea
Miguel Monzó <mikemonzo@gmail.com>
2024-12-30 12:34:07 +0100

    refactor  head and navbar

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -11,15 +13,15 @@ import (
 
 func renderTemplate(w http.ResponseWriter, tmplFile string, data interface{}) {
 	tmpl, err := template.ParseFiles(tmplFile)
-
 	if err != nil {
+		log.Fatal(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = tmpl.Execute(w, data)
-
 	if err != nil {
+		log.Fatal(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -37,6 +39,48 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if page == "" {
 		page = "index.html"
 	}
+
+	headContent, err := os.ReadFile("web/templates/header.html")
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	headTemplate, err := template.New("header").Parse(string(headContent))
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	var headBuffer bytes.Buffer
+	err = headTemplate.Execute(&headBuffer, pageData)
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	pageData.HeaderContent = template.HTML(headBuffer.String())
+
+	navbarContent, err := os.ReadFile("web/templates/navbar.html")
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	navbarTemplate, err := template.New("navbar").Parse(string(navbarContent))
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	var navbarBuffer bytes.Buffer
+	err = navbarTemplate.Execute(&navbarBuffer, pageData)
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	pageData.NavbarContent = template.HTML(navbarBuffer.String())
 
 	tmplFile := "web/templates/" + page
 
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,13 @@
 package models
 
+import "html/template"
+
 type PageData struct {
-	Title        string
-	Author       string
-	Welcome      string
-	ErrorCode    int
-	ErrorMessage string
+	Title         string
+	Author        string
+	Welcome       string
+	ErrorCode     int
+	ErrorMessage  string
+	HeaderContent template.HTML
+	NavbarContent template.HTML
 }
