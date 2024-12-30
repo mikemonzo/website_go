b7d8df08564534e209f515295ad0ac6038c65c70
Miguel Monzó <mikemonzo@gmail.com>
2024-12-30 10:08:12 +0100

    add config read

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	HTTPPort string
+}
+
+func LoadConfig() *Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AddConfigPath("./web/config/")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	cfg := &Config{
+		HTTPPort: viper.GetString("http.port"),
+	}
+
+	return cfg
+}
